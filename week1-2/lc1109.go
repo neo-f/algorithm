f712f4cc361f0b2c835658b8ece721894c2de8cf
Tian Pengfei <itsneo1990@gmail.com>
2021-06-23 01:57:56 +0800

    1109. 航班预订统计

--- week1-2/lc1109.go
+++ week1-2/lc1109.go
@@ -1,7 +1,17 @@
 package week12
 
-// https://leetcode-cn.com/problems/corporate-flight-bookings/
+// 1109. 航班预订统计 https://leetcode-cn.com/problems/corporate-flight-bookings/
 func corpFlightBookings(bookings [][]int, n int) []int {
-	// TODO
-	return nil
+	// 差分数组分别在头尾各多开一个空间
+	delta := make([]int, n+2)
+	for _, book := range bookings {
+		delta[book[0]] += book[2]
+		delta[book[1]+1] -= book[2]
+	}
+	// 累加数组也要多开一个空间
+	acc := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		acc[i] = acc[i-1] + delta[i]
+	}
+	return acc[1:]
 }
--- week1-2/week1_2_test.go
+++ week1-2/week1_2_test.go
@@ -6,16 +6,19 @@ import (
 )
 
 func TestLC1(t *testing.T) {
+}
+func TestLC11(t *testing.T)  {}
+func TestLC15(t *testing.T)  {}
+func TestLC42(t *testing.T)  {}
+func TestLC53(t *testing.T)  {}
+func TestLC84(t *testing.T)  {}
+func TestLC167(t *testing.T) {}
+func TestLC239(t *testing.T) {}
+func TestLC304(t *testing.T) {}
+func TestLC560(t *testing.T) {}
+func TestLC1109(t *testing.T) {
+	assert.Equal(t, corpFlightBookings([][]int{{1, 2, 10}, {2, 3, 20}, {2, 5, 25}}, 5), []int{10, 55, 45, 25, 25})
+}
+func TestLC1248(t *testing.T) {
 	assert.Equal(t, numberOfSubarrays([]int{1, 1, 2, 1, 1}, 3), 2)
 }
-func TestLC11(t *testing.T)   {}
-func TestLC15(t *testing.T)   {}
-func TestLC42(t *testing.T)   {}
-func TestLC53(t *testing.T)   {}
-func TestLC84(t *testing.T)   {}
-func TestLC167(t *testing.T)  {}
-func TestLC239(t *testing.T)  {}
-func TestLC304(t *testing.T)  {}
-func TestLC560(t *testing.T)  {}
-func TestLC1109(t *testing.T) {}
-func TestLC1248(t *testing.T) {}
