ef4a2752ea5da7010bceafd1e2e727de20d24f2a
Tian Pengfei <itsneo1990@gmail.com>
2021-08-05 11:50:35 +0800

    add weekly desc

--- gen.py
+++ gen.py
@@ -5,15 +5,28 @@ from dataclasses import dataclass
 from typing import List
 
 GROUPS = dict(
-    week1="88,21,26,283,66,206,20,155,150,641,141,227,560,25,1248,304,1109,53,"
-          + "1,167,15,11,84,239,42,560",
-    week2="874,49,30,146,78,77,46,111,104,226,98,50,22",
-    week3="106,210,130,94,589,429,297,105,1245,236,684,207,17,51,200,433,329",
-    week4="23,295,355,239,704,410,162,34",
-    week5="912,1122,56,215,493,322,860,455,122,45,1665,1011,911,875,327",
-    week6="322,63,1143,300,53,152,121,122,123,188,714,309,198,213,72,416,518,"
-          + "70,120,673,279,55,45",
-    week7="1499,1000,312,918,300,684,200"
+    week1={
+        "desc": "数组、链表、栈、队列、前缀和、差分、双指针扫描",
+        "items": "88,21,26,283,66,206,20,155,150,641,141,227,560,25,1248,304,1109,53,1,167,15,11,84,239,42,560",
+    },
+    week2={
+        "items": "874,49,30,146,78,77,46,111,104,226,98,50,22",
+        "desc": "哈希表、集合、映射、递归、分治",
+    },
+    week3={
+        "items": "106,210,130,94,589,429,297,105,1245,236,684,207,17,51,200,433,329",
+        "desc": "树、图、深度优先搜索、广度优先搜索",
+    },
+    week4={"items": "23,295,355,239,704,410,162,34", "desc": "二叉堆、二叉搜索树、二分"},
+    week5={
+        "items": "912,1122,56,215,493,322,860,455,122,45,1665,1011,911,875,327",
+        "desc": "排序、贪心",
+    },
+    week6={
+        "items": "322,63,1143,300,53,152,121,122,123,188,714,309,198,213,72,416,518,70,120,673,279,55,45",
+        "desc": "动态规划",
+    },
+    week7={"items": "1499,1000,312,918,300,684,200", "desc": "动态规划、Trie树、并查集"},
 )
 
 TITLE = "# Leetcode Solutions\n\n"
@@ -55,10 +68,8 @@ def _parse_week(nums: str) -> List[Item]:
 
 
 def _gen_statistic() -> str:
-    content = (
-            f"> Generate Time: {datetime.date.today()}\t"
-            + f"(Problems : {STATISTIC['problems']})\n"
-    )
+    content = f"> Generate Time: {datetime.date.today()}\t"
+    content += f"(Problems : {STATISTIC['problems']})\n"
     for lang, num in STATISTIC["lang"].items():
         content += f"> + Solutions({lang}): {num}\n"
     return content
@@ -71,20 +82,21 @@ DIFFICULTY = {
 }
 
 
-def _gen_week(week: str, items: List[Item]) -> str:
+def _gen_week(week: str, desc: str, items: List[Item]) -> str:
     ret = "***\n"
-    ret += f"## {week.capitalize()}\n\n"
+    ret += f"## {week.capitalize()}\n"
+    ret += f"> {desc}\n\n"
     titles = ["No.", "题目", "难度", "Tags", "Solutions"]
     ret += f"|{'|'.join(titles)}|\n"
     ret += "|---|---|---|---|---|\n"
     for idx, item in enumerate(items, start=1):
-        line = []
-        line.append(str(idx))
-        link = f"[{item.title}](https://leetcode-cn.com/problems/{item.slug})"
-        line.append(link)
-        line.append(f"{DIFFICULTY[item.difficulty]}")
-        line.append(" ".join([f"`{t}`" for t in item.tags]))
-        line.append(_gen_solutions_str(item.solutions_dir))
+        line = [
+            str(idx),  # No.
+            f"[{item.title}](https://leetcode-cn.com/problems/{item.slug})",  # Link
+            f"{DIFFICULTY[item.difficulty]}",  # Difficulty
+            " ".join([f"`{t}`" for t in item.tags]),  # Tags
+            _gen_solutions_str(item.solutions_dir),  # Solutions
+        ]
         ret += f"|{'|'.join(line)}|\n"
     ret += "\n\n"
     return ret
@@ -124,8 +136,8 @@ def _gen_solutions_str(solutions_dir: str) -> str:
 
 def main():
     content = ""
-    for week, nums in GROUPS.items():
-        content += _gen_week(week, _parse_week(nums))
+    for week, item in GROUPS.items():
+        content += _gen_week(week, item["desc"], _parse_week(item["items"]))
 
     with open("README.md", "w") as readme:
         readme.write(TITLE)
